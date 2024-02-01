722b39241734aba1dc9e871b09621e0148e03e7b
Ashwin-3cS <flashashwin2004@gmail.com>
2024-02-01 17:52:55 +0530

    name changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
   return (
     <div className=' flex flex-col	items-center justify-center	 text-center	h-screen	login  ' >
     <div className='login_container'>
-      <h1 className='logo'>BEING HUMAN</h1>
+      <h1 className='logo'>XX APP</h1>
       <form onSubmit={handleSubmit} className="login-form">
           <input className=" phSignupLogin placeholder:italic placeholder:text-slate-50 block  w-full border border-cyan-600 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             type="email"
